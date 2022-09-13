1ea0e9e3dd6b3a6118c8082ab5880f521cedffbe
KevinMartinezC <90873838+KevinMartinezC@users.noreply.github.com>
2022-09-12 20:29:03 -0600

    Creacion de menu y stacks de cada una de las screen

---
+++ src/navigation/RestaurantStack.js
@@ -0,0 +1,24 @@
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {RestauranstScreen} from "../screens/Restaurants/RestauranstScreen";
+import {AddRestaurantScreen} from "../screens/Restaurants/AddRestaurantScreen";
+
+import {screen} from "../utils";
+
+const Stack = createNativeStackNavigator();
+
+export function RestaurantStack(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen 
+            name={screen.restaurant.restaurants} 
+            component={RestauranstScreen}
+            options={{title: "Restaurantes"}}
+            />
+            <Stack.Screen 
+            name={screen.restaurant.addRestaurant} 
+            component={AddRestaurantScreen}
+            options={{title: "Nuevo Restaurante"}}
+            />
+        </Stack.Navigator>
+    )
+}
---
+++ src/utils/screenName.js
@@ -0,0 +1,32 @@
+const restaurantStack = {
+    tab: "RestaurantsTab",
+    restaurants: "Restaurants",
+    addRestaurant:"AddRestaurant",
+};
+const favoritsStack = {
+    tab: "FavoritesTab",
+    favorites: "Favorites",
+   
+};
+const rankingStack = {
+    tab: "RankingTab",
+    ranking: "Ranking",
+};
+const searchStack = {
+    tab: "SearchTab",
+    search: "Search",
+};
+const accountStack = {
+    tab: "AccountTab",
+    account: "Account",
+};
+
+export const screen = {
+    restaurant: restaurantStack,
+    favorites: favoritsStack,
+    ranking : rankingStack,
+    search: searchStack,
+    account: accountStack,
+
+};
+
