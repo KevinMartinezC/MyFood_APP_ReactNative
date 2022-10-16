755f4265f76e2f4232b911cf40c6a45cde4bf063
kevin martinez <ceronk003@gmail.com>
2022-10-15 20:26:20 -0600

    Comprobar si el restaurante ya se encuentra en nuestra lista de favoritos

--- src/components/Restaurant/BtnFavorite/BtnFavorite.js
+++ src/components/Restaurant/BtnFavorite/BtnFavorite.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import { View, Text } from 'react-native';
 import {Icon} from "react-native-elements";
 import {getAuth} from "firebase/auth";
 import {doc, setDoc,getDocs, collection, query, where, deleteDoc} from "firebase/firestore";
 import {v4 as uuid} from "uuid";
+import {size} from "lodash";
 import {db} from "../../../utils";
 import {styles} from "./BtnFavorite.styles";
 import { async } from '@firebase/util';
@@ -11,7 +12,34 @@ import { async } from '@firebase/util';
 
 export  function BtnFavorite(props) {
     const {idRestaurant} = props;
+    const [isFavorite, setIsFavorite] = useState(undefined);
     const auth = getAuth()
+
+
+    useEffect(() => {
+      (async () => {
+        const response = await getFavorites()
+
+       if(size(response) > 0){
+        setIsFavorite(true);
+       }else{
+        setIsFavorite(false);
+       }
+
+      })();
+    }, [idRestaurant])
+    
+    const getFavorites = async () => {
+        const q = query(
+            collection(db, "favorites"),
+            where("idRestaurant","==", idRestaurant),
+            where("idUser", "==", auth.currentUser.uid)
+        );
+
+        const result =  await getDocs(q);
+        return result.docs;
+
+    };
 
     const addFavorite = async () => {
         try {
@@ -29,16 +57,22 @@ export  function BtnFavorite(props) {
         }
     }
 
+    const removeFavorite = () => {
+        console.log("Eliminar favorito")
+    };
 
   return (
     <View style={styles.content} >
-      <Icon
-        type='material-community'
-        name='heart-outline'
-        color="#000"
-        size={35}
-        onPress={addFavorite}
-      />
+        {isFavorite !== undefined && (
+            <Icon
+                type='material-community'
+                name={isFavorite ? "heart" : "heart-outline"}
+                color={isFavorite ? "#f00" : "#000"}
+                size={35}
+                onPress={isFavorite ? removeFavorite : addFavorite}
+             />
+        )}
+      
       
     </View>
   )
