9f065d1c614b818bba9baa221f442cf3e2999fde
kevin martinez <ceronk003@gmail.com>
2022-10-16 10:50:11 -0600

    Screen Favoritos para usuario no logeado

--- src/screens/FavoritesScreen.js
+++ src/screens/FavoritesScreen.js
@@ -1,7 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { View, Text } from 'react-native';
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+import { UserNotLogged } from "../components/Favorites";
+
+
 
 export  function FavoritesScreen() {
+  const [hasLogged, setHasLogged] = useState(null);
+  const auth = getAuth();
+
+  useEffect(() => {
+   onAuthStateChanged(auth, (user) =>{
+    setHasLogged(user ? true : false);
+   })
+  }, [])
+  
+  if(!hasLogged) return <UserNotLogged/>
+
   return (
     <View>
       <Text>FavoritesScreen</Text>
