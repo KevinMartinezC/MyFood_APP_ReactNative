1639d0edc80e6b7d415d44dd0190c8d7b2a568e5
kevin martinez <ceronk003@gmail.com>
2022-10-15 19:46:14 -0600

    añadiendo restaurantes a favoritos en firebase

--- src/components/Restaurant/BtnFavorite/BtnFavorite.js
+++ src/components/Restaurant/BtnFavorite/BtnFavorite.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import {Icon} from "react-native-elements";
+import {getAuth} from "firebase/auth";
+import {doc, setDoc,getDocs, collection, query, where, deleteDoc} from "firebase/firestore";
+import {v4 as uuid} from "uuid";
+import {db} from "../../../utils";
 import {styles} from "./BtnFavorite.styles";
+import { async } from '@firebase/util';
 
 
 export  function BtnFavorite(props) {
     const {idRestaurant} = props;
+    const auth = getAuth()
 
-    const addFavorite = () => {
-        console.log("Anadir a favoritos")
+    const addFavorite = async () => {
+        try {
+            const idFavorite = uuid();
+           const data = {
+            id: idFavorite,
+            idRestaurant,
+            idUser: auth.currentUser.uid
+           };
+
+           await setDoc(doc(db, "favorites", idFavorite), data);
+           
+        } catch (error) {
+            console.log(error);
+        }
     }
 
 
